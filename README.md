2b8c0bb8cea5ba8a0b8519e464650fdafbd02041
Recoilless Interactive <68288735+re-coilless@users.noreply.github.com>
2026-06-12 02:46:45 +0400

    finally figured out

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Hardcore combat focused gamemode with tiered exploration and crafting system.
 
-[PenMan](https://github.com/re-coilless/penman), [HermeS Vector](https://steamcommunity.com/sharedfiles/filedetails/?id=3740443927), [HermeS Marshall](https://steamcommunity.com/sharedfiles/filedetails/?id=3064506772) and [Worse Enemies](https://steamcommunity.com/sharedfiles/filedetails/?id=2506744865) are required.
+[PenMan](https://github.com/re-coilless/penman), [HermeS Index](https://steamcommunity.com/sharedfiles/filedetails/?id=3740447207), [HermeS Marshall](https://steamcommunity.com/sharedfiles/filedetails/?id=3064506772) and [Worse Enemies](https://steamcommunity.com/sharedfiles/filedetails/?id=2506744865) are required.
--- hiisi_massacre/init.lua
+++ hiisi_massacre/init.lua
@@ -12,6 +12,16 @@ function OnModInit()
 		></Biome>
 	]])
 	pen.magic_write( "data/biome/_biomes_all.xml", tostring( xml ))
+
+	--custom ost through marshall
+	--do rings with gates that are open for now
+	--lua walls should be a vector module
+
+	--enemy spawning
+	--allow trading unique currency for hp, wands and spells
+
+	--ability to transfer equipment between saves
+	--the main resource is the powder absorbed by player that is flammable and is dissolved on contact with water (add new ui element for it)
 end
 
 --hub area
@@ -19,27 +29,28 @@ end
 	--dialogue should be in-world and narrated through talk-it (use vector module option)
 	--crafting system for custom perks and plot items (use both pixel materials and items on the floor)
 	--shop for spells
-	--ability to transfer equipment between saves
 --mapgen
 	--made from sparematter (penman should inject this)
 	--10 circles, with each one requiring progressively harder to obtain unlock items/conditions
 	--first 5 use vanilla enemies, the latter 5 introduce worse enemies
 	--escaping and providing rare materials allows one to repair the shop antenna (this turns shop 100% free)
-	--custom ost through marshall
 	--hybrid gen rules (use wang for baseline shape and then manually spawn in room geometries from a pool)
 	--add a map that is created during the terrain gen step
 --rooms
+	--every room has a marker at the center that controls the spawns (the actual spawns are being done from init.lua)
 	--horizontal room should have their cielings patched with phantom platforms
-	--locked doors with keys in containers
+	--locked doors with keys in containers (keys are universal)
 	--dead ends must always be locked and always have loot
-	--do pathfinding on the obtained map and unlock a random door if one cannot explore the entire region
 	--locked or optinally locked rooms should have only one true entrance and one true exit
 	--add bg doors that teleport between two locations (unlocked by pulling a lever at both sides)
 	--archetypes: combat, storage, hallways, utility, volatile
-	--variants: easter eggs, ruins, ambush (enemy spawns have a chance to being retriggered), prison, armory
+	--variants: funny, ruins, ambush (enemy spawns have a chance to being retriggered), prison, armory
+	--circles: Prologue (normal), Wastes (toxic), Abyss (flooded), Ruins (crumbling), Gehenna (burning with smoke everywhere), Crux (no enemies spawn statically, they come out of portals that open randomly + blaring alarms), Buffer (lots of turrents and heavily fortified positions), Masquerade (normal enemies are bursting into abominations that are hostile to everything), Noose (bosses everywhere), Gates (very different enemies + all doors are always locked)
+	--gates are always located in the same spots, rotated 180 between each one (the initial orientation is randomized)
 --unique items
-	--return gate
+	--return gate (creates an explosion where player was, can be used to cheese doors)
 	--marker beacons + chalk
 	--concrete sprayer
 	--breach charge
 	--supply crate
+	--teleporter
